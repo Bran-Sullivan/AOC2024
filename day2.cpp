e00323790031a12d3c3be3d638d00f33b0501ad7
psybs9 <psybs9@nottingham.ac.uk>
2024-12-02 12:04:02 +0000

    day2 complete

--- day2.cpp
+++ day2.cpp
@@ -37,43 +37,34 @@ int d2p1(){
 }
 
 int d2p2(){
-    int total = 0, num1, num2, inc,tempinc = 0,dif, safe, i;
+    int total = 0, num1, safe, i;
     std::string line;
-
     while(std::getline(std::cin, line)){
-        safe = 1;
-        inc = 0;
-        i=1;
+        safe = 0;
+        i=0;
         std::istringstream stream(line);
-        stream >> num1;
-        safe = checkline(line,0);
-        while (stream >> num2){
-            if(num1 > num2){tempinc = -1;}
-            else if(num1 < num2){tempinc = 1;}
-            else{tempinc = 0;}
-            if(inc == 0){inc = tempinc;}
-            dif = std::abs(num1 - num2);
-            if(dif > 3 || dif == 0 || tempinc != inc){
-                safe = checkline(line,i);
+        while (stream >> num1){
+            safe = checkline(line,i);
+            if(safe==1){
+                break;
             }
             i++;
-            num1 = num2;
         }
         total += safe;
-        std::cout << safe << "\n";
+        std::cout << safe;
     }
     return total;
 }
 
 int checkline(std::string line, int ignore){
-    int num1, num2, inc,tempinc = 0,dif, safe=1, i = 1;
+    int num1, num2, inc=0,tempinc = 0,dif, safe=1, i = 1;
     std::istringstream stream(line);
     if(ignore == 0){stream >> num1;}
     stream >> num1;
     while (stream >> num2){
-        std::cout<< i <<"ignore: " <<ignore << "\n";
         if(i == ignore){
-            stream >> num2;
+            i++;
+            continue;
         }
         if(num1 > num2){tempinc = -1;}
         else if(num1 < num2){tempinc = 1;}
@@ -86,6 +77,5 @@ int checkline(std::string line, int ignore){
         num1 = num2;
         i++;
     }
-    std::cout << safe << "\n";
     return safe;
 }
